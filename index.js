24bdd7acc0d0dac11c041a5267a69d15a2bd51d3
Rahul Saha <rahulsaha87928@gmail.com>
2023-07-07 21:46:38 +0600

    get single data from database

--- index.js
+++ index.js
@@ -37,6 +37,13 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/tasks/:id', async(req, res) => {
+          const id = req.params.id;
+          const query = {_id: new ObjectId(id)}
+          const result = await taskCollection.findOne(query);
+          res.send(result);
+      })
+
         app.post('/tasks', async (req, res) => {
             const task = req.body;
             console.log('new task', task);
