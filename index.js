5612a3f3fabf1107cac0388846dd9062093ab88a
Rahul Saha <rahulsaha87928@gmail.com>
2023-07-07 12:52:28 +0600

    setup server

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get('/', (req, res) => {
+    res.send(`Task sever is running`)
+})
+
+app.listen(port, () => {
+    console.log(`Task server running on port, ${port}`);
+})
