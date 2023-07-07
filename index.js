a45381cd73012ba2e0167a92ec1cf8c13ce817f3
Rahul Saha <rahulsaha87928@gmail.com>
2023-07-07 21:24:18 +0600

    delete method done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 require('dotenv').config()
@@ -43,6 +43,15 @@ async function run() {
             const result = await taskCollection.insertOne(task);
             res.send(result);
         })
+
+
+        app.delete('/tasks/:id', async(req, res) => {
+          const id = req.params.id;
+          console.log('please delete from database', id);
+          const query = {_id: new ObjectId(id)};
+          const result = await taskCollection.deleteOne(query)
+          res.send(result);
+      })
 
 
 
