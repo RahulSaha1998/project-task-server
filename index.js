e2325ab1d8a2cb6991623a699eec4a036db72f5b
Rahul Saha <rahulsaha87928@gmail.com>
2023-07-08 21:46:17 +0600

    implement get api by email

--- index.js
+++ index.js
@@ -32,66 +32,86 @@ async function run() {
     const taskCollection = client.db('toDoListProject').collection("tasks");
     const userCollection = client.db('toDoListProject').collection("users");
 
-        app.get('/tasks', async (req, res) => {
-            const cursor = taskCollection.find()
-            const result = await cursor.toArray();
-            res.send(result)
-        })
+    app.get('/tasks', async (req, res) => {
+      const cursor = taskCollection.find()
+      const result = await cursor.toArray();
+      res.send(result)
+    })
 
-        //For single task data
-        app.get('/tasks/:id', async(req, res) => {
-          const id = req.params.id;
-          const query = {_id: new ObjectId(id)}
-          const result = await taskCollection.findOne(query);
-          res.send(result);
-      })
+    //For single task data
+    app.get('/tasks/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await taskCollection.findOne(query);
+      res.send(result);
+    })
 
-        app.post('/tasks', async (req, res) => {
-            const task = req.body;
-            console.log('new task', task);
-            const result = await taskCollection.insertOne(task);
-            res.send(result);
-        })
+    app.post('/tasks', async (req, res) => {
+      const task = req.body;
+      console.log('new task', task);
+      const result = await taskCollection.insertOne(task);
+      res.send(result);
+    })
 
-        app.delete('/tasks/:id', async(req, res) => {
-          const id = req.params.id;
-          console.log('please delete from database', id);
-          const query = {_id: new ObjectId(id)};
-          const result = await taskCollection.deleteOne(query)
-          res.send(result);
-      })
+    app.delete('/tasks/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log('please delete from database', id);
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.deleteOne(query)
+      res.send(result);
+    })
 
-      app.post('/users', async (req, res) => {
-        const user = req.body;
-        const query = { email: user.email }
-        const existingUser = await userCollection.findOne(query);
-
-        if (existingUser) {
-            return res.send({ message: 'user already exists' })
+    app.put('/tasks/:id', async (req, res) => {
+      const id = req.params.id;
+      const updatedTask = req.body;
+      console.log(id, updatedTask);
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true }
+      const updatedUser = {
+        $set: {
+          title: updatedTask.title,
+          date: updatedTask.date,
+          status: updatedTask.status,
+          description: updatedTask.description
         }
+      }
 
-        const result = await userCollection.insertOne(user);
-        res.send(result);
+      const result = await taskCollection.updateOne(filter, updatedUser, options);
+      res.send(result);
+    })
+
+    //Post new user to the database
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query);
+
+
+      if (existingUser) {
+        return res.send({ message: 'user already exists' })
+      }
+
+      const result = await userCollection.insertOne(user);
+      res.send(result);
     });
 
-      app.put('/tasks/:id', async(req, res) => {
-        const id = req.params.id;
-        const updatedTask = req.body;
-        console.log(id, updatedTask);
-        const filter = {_id: new ObjectId(id)}
-        const options = {upsert:true}
-        const updatedUser = {
-            $set: {
-                title: updatedTask.title,
-                date: updatedTask.date,
-                status: updatedTask.status,
-                description: updatedTask.description
-            }
-        }
+    //get all users
+    app.get('/users', async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result)
+    })
 
-        const result = await taskCollection.updateOne(filter, updatedUser, options );
-        res.send(result);
-      })
+    //user get by email address
+    app.get('/users/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email }
+      const user = await userCollection.findOne(query);
+      const result = { admin: user?.role === 'admin' }
+      res.send(result);
+    })
+
+
+
 
 
 
@@ -110,9 +130,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send(`Task sever is running`)
+  res.send(`Task sever is running`)
 })
 
 app.listen(port, () => {
-    console.log(`Task server running on port, ${port}`);
+  console.log(`Task server running on port, ${port}`);
 })
